6510b3a40e553b486b798ab79d61f17ead7b1c09
Ahmed Maklad <ahmed.mostafa.makled@gmail.com>
2024-11-30 20:28:27 +0200

    Update upstream with my main branch

--- AllBirds.AdminDashboard/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function closeMsg(evt) {
-    let parent = evt.target.children.length ? evt.target.parentElement : evt.target.parentElement.parentElement;
-    console.log(parent);
-    parent.setAttribute("style", "display:none;");
-}
